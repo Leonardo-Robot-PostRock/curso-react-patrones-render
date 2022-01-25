75a085981aacc6997b4303a4828d9da24656cf3e
Leonardo Puebla <leonardo38361@gmail.com>
2022-01-25 20:14:22 -0300

    composición de componentes y colocación de estados

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,52 @@ import React from 'react';
 import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 
-
+//COMPOSICIÓN DE COMPONENTES
 function App() {
+  const [state, setState] = React.useState('estado compartido');
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+
+      <TodoList>
+        <TodoItem state={state} />
+      </TodoList>
+    </React.Fragment>
   );
 }
+function TodoHeader({ children }) {
+  return (
+    <header>
+      {children}
+    </header>
+  );
+}
+function TodoList({ children }) {
+  return (
+    <section className='TodoList-container'>
+      {children}
+    </section>
+  );
+}
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+function TodoSearch() {
+  return <p>TodoSearch </p>;
+}
+function TodoItem({ state }) {
+  return <p>TodoItem: {state}</p>;
+}
+
+// function Ap() {
+//   return (
+//     <Todoprovider>
+//       <ApUI />
+//     </Todoprovider>
+//   );
+// }
 
 export default App;
